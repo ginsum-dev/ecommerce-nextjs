0fb04ee7c153452663740f5bafde5591023bf999
ginsum-dev <jennyjung.dev@gmail.com>
2025-07-08 22:31:15 +0900

    Feat: Header, Navbar 생성

---
+++ src/components/custom/Header.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import arrowLeftIcon from '@/assets/icons/arrow-left-icon.svg';
+
+export default function Header({
+  children,
+  backPath,
+}: {
+  children: React.ReactNode;
+  backPath?: string;
+}) {
+  return (
+    <header className="relative flex justify-center items-center p-4">
+      {backPath && (
+        <div className="absolute left-5">
+          <Link href={backPath}>
+            <Image
+              src={arrowLeftIcon}
+              alt="arrow-left"
+              width={16}
+              height={16}
+            />
+          </Link>
+        </div>
+      )}
+      {children}
+    </header>
+  );
+}
---
+++ src/components/custom/Navbar.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import cartIcon from '@/assets/icons/cart-icon.svg';
+
+export default function Navbar() {
+  return (
+    <div className="flex gap-4 absolute right-5">
+      <Link href="/cart">
+        <Image src={cartIcon} alt="cart" width={24} height={24} />
+      </Link>
+    </div>
+  );
+}
